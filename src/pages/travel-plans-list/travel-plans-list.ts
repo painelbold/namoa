aea24d344c35d0539ca9767b00bf1774ce1c9408
App Painel Bold <app.painelbold@gmail.com>
2018-08-02 00:51:16 -0300

    Loading para listagem de

--- src/pages/travel-plans-list/travel-plans-list.ts
+++ src/pages/travel-plans-list/travel-plans-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Loading, LoadingController, Content } from 'ionic-angular';
 
 import { TravelPlanPage } from '../plan/travel-plan/travel-plan';
 import { TravelPlan } from './../../models/travelPlan';
@@ -14,20 +14,32 @@ import { TravelPlanDetailPage } from '../travel-plan-detail/travel-plan-detail';
 export class TravelPlansListPage {
   travelPlans: Array<TravelPlan>;
   cities: Array<string>;
+  loading: Loading;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    private tpProvider: TravelPlanProvider) {
+    private tpProvider: TravelPlanProvider,
+    private loadingCtrl: LoadingController) {
       this.travelPlans = new Array<TravelPlan>();
       this.cities = new Array<string>();
   }
 
   ionViewDidLoad() {
+    this.createLoading();
     const subscribe = this.tpProvider.getAll(localStorage.getItem("loggedUserKey"))
     .subscribe((tp: any) =>{
         this.travelPlans = tp;
+        this.loading.dismiss();
         subscribe.unsubscribe();
     });
+  }
+
+  createLoading(){
+    this.loading = this.loadingCtrl.create({
+      content: "Carregando planos..."
+    }
+    );
+    this.loading.present();
   }
 
   newTravelPlan(){
